821cc91c79f528f017622b51bfb12c5b87342306
Ahmed-Fathy-Abkarenoo <a_abomosalam@hotmail.com>
2025-12-15 23:03:48 +0200

    check some thing

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -11,7 +11,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           <header className="p-4">
             <BreadCrumb />
           </header>
-
           <main className="h-full p-5 space-y-4 bg-gray-50">{children}</main>
         </SidebarInset>
       </SidebarProvider>
